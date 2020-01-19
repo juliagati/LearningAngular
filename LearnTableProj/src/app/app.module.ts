62ae19246f352b3d399a2908fb97f668360a96fc
juliagati <juliagati241@gmail.com>
2020-01-19 19:35:26 +0200

    commit to my branch juljul

--- LearnTableProj/src/app/app.module.ts
+++ LearnTableProj/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TableComponentComponent } from './table-component/table-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponentComponent
   ],
   imports: [
     BrowserModule
